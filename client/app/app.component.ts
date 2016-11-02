f8627d2d2a478d2c0f6c21d00d659682967ddfc4
Sergei <Sergei>
2016-11-02 16:01:47 +0300

    Add eshopper website template

--- client/app/app.component.ts
+++ client/app/app.component.ts
@@ -1,10 +1,10 @@
 import {Component} from '@angular/core';
 
 @Component({
-    selector:'my-app',
+    selector:'books-app',
     templateUrl: './app/app.html'
 })
 
 export class AppComponent {
-    title = 'Tour of Heroes';
+    title = 'Book store';
 }
