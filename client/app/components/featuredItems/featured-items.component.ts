c5586d579500736be25196f619060400ac2e1583
Sergei <Sergei>
2016-11-03 17:58:03 +0300

    Clean up book detail

--- client/app/components/featuredItems/featured-items.component.ts
+++ client/app/components/featuredItems/featured-items.component.ts
@@ -1,8 +1,11 @@
-import {Component} from '@angular/core';
+import {Component, Input} from '@angular/core';
+import {Book} from './../../models/book';
 
 @Component({
     selector: 'featured-items',
     templateUrl: './app/components/featuredItems/featured-items.component.html'
 })
 export class FeaturedItemsComponent {
+    @Input() books: Book[] = [];
+
 }
