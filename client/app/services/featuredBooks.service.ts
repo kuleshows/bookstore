ef0d5b649704ce1fb174d7c129da5ebd52bb5474
Sergei <Sergei>
2016-11-10 18:04:14 +0300

    Correct date for featured books

--- client/app/services/featuredBooks.service.ts
+++ client/app/services/featuredBooks.service.ts
@@ -3,6 +3,8 @@ import {Headers, Http, Response} from '@angular/http';
 import 'rxjs/add/operator/toPromise';
 import {Book} from './../models/book';
 
+declare var moment: any;
+
 @Injectable()
 export class FeaturedBooksService {
     private featuredBooksUrl: string = 'api/featuredbooks';
@@ -10,7 +12,7 @@ export class FeaturedBooksService {
     constructor(private http: Http) {}
 
     getBooks(date: Date): Promise<Book[]> {
-        return this.http.get(`${this.featuredBooksUrl}/2016-11-10`)
+        return this.http.get(`${this.featuredBooksUrl}/${moment(date).add(-10, 'days').format('YYYY-MM-DD')}`)
             .toPromise()
             .then(response => response.json())
             .catch(this.handleError);
